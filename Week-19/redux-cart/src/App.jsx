635746a3c6a543b35acf632f3d517322021fd40d
Zeenat <zeenatkm301@gmail.com>
2023-09-16 11:16:56 -0700

    redux assignment 2 cart page done

---
+++ Week-19/redux-cart/src/App.jsx
@@ -0,0 +1,14 @@
+
+import "./App.css";
+import ShoppingCart from "./pages/ShoppingCart";
+
+function App() {
+  return (
+    <>
+      <h1>My E-Commerce App</h1>
+      <ShoppingCart />
+    </>
+  );
+}
+
+export default App;
