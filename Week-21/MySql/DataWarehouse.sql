abcce2ec05107d8f5cbce874bbfc981d09a31040
Zeenat <zeenatkm301@gmail.com>
2023-10-24 12:48:10 +0530

    sql assigmnet done

---
+++ Week-21/MySql/DataWarehouse.sql
@@ -0,0 +1,24 @@
+#Find the different warehouses in "Pune".
+SELECT DISTINCT WNAME 
+FROM WAREHOUSES 
+WHERE LOCATION = 'Pune'; 
+
+#Find a Warehouse that has the maximum number of stores.
+SELECT WID, WNAME, COUNT(*) AS StoreCount
+FROM WAREHOUSES w
+JOIN STORE s ON w.WID = s.SID
+GROUP BY WID, WNAME
+ORDER BY StoreCount DESC
+LIMIT 1;
+
+#Find the item that has the minimum weight.
+SELECT * FROM ITEMS
+WHERE WEIGHT = (SELECT MIN(WEIGHT) FROM ITEMS);
+
+#Find an item that is ordered for a minimum number of times.
+SELECT i.ITEMNO, i.DESCRIPTION, COUNT(o.ONO) AS OrderCount
+FROM ITEMS i
+LEFT JOIN ORDERS o ON i.ITEMNO = o.ONO
+GROUP BY i.ITEMNO, i.DESCRIPTION
+ORDER BY OrderCount ASC
+LIMIT 1;
